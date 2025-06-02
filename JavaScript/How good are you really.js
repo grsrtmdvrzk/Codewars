9fe96ca595e1a44b207f55e2a2bd69e68494093f
grsrtmdvrzk <grsrtmdvrzk@gmail.com>
2025-06-02 13:31:03 +0400

    Add files via upload

---
+++ JavaScript/How good are you really.js
@@ -0,0 +1,26 @@
+// TASK
+// There was a test in your class and you passed it. Congratulations!
+// But you're an ambitious person. You want to know if you're better than the average student in your class.
+// You receive an array with your peers' test scores. Now calculate the average and compare your score!
+// Return true if you're better, else false!
+//     Notes
+//         Your points are not included in the array of your class's points. Do not forget them when calculating the average score!
+
+// SOLUTION
+function betterThanAverage(classPoints, yourPoints) {
+    getAverage = (classPoints) => {
+        let sum = 0;
+
+        for (let i = 0; i < classPoints.length; i += 1) {
+            sum += classPoints[i];
+        }
+
+        return (sum + yourPoints) / (classPoints.length + 1);
+    }
+
+    if (yourPoints > getAverage(classPoints)) {
+        return true;
+    } else {
+        return false;
+    }
+}
---
+++ JavaScript/Reversed Strings.js
@@ -0,0 +1,15 @@
+// TASK
+// Complete the solution so that it reverses the string passed into it.
+//
+//     Examples
+//         'world'  =>  'dlrow'
+//         'word'   =>  'drow'
+
+// SOLUTION
+function solution(str){
+    let reversed = '';
+    for (let i = str.length - 1; i >= 0; i--) {
+        reversed += str[i]
+    }
+    return reversed;
+}
